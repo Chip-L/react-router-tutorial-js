5c7440ef98cb50e7b63a369088db9e6fbd6e6a3b
Chip Long <chip_long@yahoo.com>
2023-08-25 11:42:03 -0600

    nested routes

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,3 +1,5 @@
+import { Outlet } from "react-router-dom";
+
 const Root = () => (
   <>
     <div id="sidebar">
@@ -29,7 +31,9 @@ const Root = () => (
         </ul>
       </nav>
     </div>
-    <div id="detail"></div>
+    <div id="detail">
+      <Outlet />
+    </div>
   </>
 );
 
